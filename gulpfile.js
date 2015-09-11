3ff091727d649db39db71c1fb9c5e21c2cf86c03
Troy Mullaney <troym9731@gmail.com>
2015-09-11 16:55:25 -0700

    Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var rimraf = require('rimraf');
+var serve = require('gulp-serve');
+var sass = require('gulp-sass');
+
+/****************************************
+  JS
+*****************************************/
+
+var bundler = browserify({
+  entries: ['./src/app.js'],
+  debug: true
+});
+
+bundler.on('log', gutil.log); // output build logs to terminal
+
+gulp.task('clean', function (cb) {
+  rimraf('build', cb);
+});
+
+gulp.task('build', ['clean'], function () {
+  return bundler.bundle()
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    .pipe(babel())
+    .pipe(gulp.dest('build'));
+});
+
+
+/****************************************
+  Servers (Web and API)
+*****************************************/
+
+gulp.task('serve', serve({
+  root: ['.'],
+  port: process.env.PORT || 8000
+}));
+
+
+/****************************************
+  Sass
+*****************************************/
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+/****************************************
+  Watch
+*****************************************/
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch('src/**/*.js', ['build']);
+  gulp.watch('./sass/*.scss', ['sass'])
+})
+
+// Default
+gulp.task('default', ['serve', 'watch', 'sass']);
