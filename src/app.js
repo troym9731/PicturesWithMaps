e7a9b3847d35bbf8d8f55fdd41bfd5f8fecf4d4a
Troy Mullaney <troym9731@gmail.com>
2015-09-13 12:01:52 -0700

    Add font-awesome

--- src/app.js
+++ src/app.js
@@ -4,4 +4,13 @@ import fmServices from './services';
 import fmControllers from './controllers';
 
 let fmApp = angular.module('fmApp',
-  ['ngRoute', 'fmControllers', 'fmServices']);
+  ['ngRoute']);
+
+fmApp.config(
+  ['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: './src/partials/home.html'
+      })
+  }]
+)
