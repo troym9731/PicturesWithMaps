f205ca3af95308d32254741c05ff0e1104a342fa
Troy Mullaney <troym9731@gmail.com>
2015-09-14 12:38:57 -0700

    Transition to /map

--- src/controllers.js
+++ src/controllers.js
@@ -10,13 +10,25 @@ fmControllers.controller('LoginCtrl', ['$scope', '$location',
       OAuth.initialize('4lBI76EZhVMz17EBWkTmYb0mg_4')
       OAuth.popup('instagram').done(function(res) {
           console.log(res);
-          res.me().done(function(me) {
-            console.log(me);
+          console.log(res.user);
+          $scope.$apply(function() {
             $location.path('/map');
           });
-          // do some stuff with result
+
       }).fail(function(err) {
         console.log(err);
+      });
+    }
+  }
+])
+
+fmControllers.controller('MapCtrl', ['$scope',
+  function($scope) {
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        scrollwheel: false,
+        zoom: 8
       });
     }
   }
