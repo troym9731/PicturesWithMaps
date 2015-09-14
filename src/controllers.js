d97a665e76fafdba258ab6d6f86e481f9853cb89
Troy Mullaney <troym9731@gmail.com>
2015-09-13 19:38:42 -0700

    Minor changes

--- src/controllers.js
+++ src/controllers.js
@@ -9,13 +9,7 @@ fmControllers.controller('LoginCtrl', ['$scope',
     $scope.login = function() {
       OAuth.popup('flickr', { cache: true }).done(function(res) {
         //make API calls with `res`
-        console.log(res);
-        res.me().done(function(me) {
-          console.log('Hello ' + me.name);
-        }).fail(function(err) {
-          console.log(err);
-          //todo when the OAuth flow failed
-        });
+        
       }).fail(function(err) {
         //todo when the OAuth flow failed
         console.log(err);
--- src/services.js
+++ src/services.js
@@ -3,4 +3,15 @@ import angular_resource from 'angular-resource';
 
 let fmServices = angular.module('fmServices', ['ngResource']);
 
+fmServices.factory('User', ['$resource',
+  function($resource) {
+    return $resource('http://api.flickr.com/services/rest/', {}, {
+      query: {
+        method: 'GET',
+        isArray: true
+      }
+    })
+  }
+])
+
 export default fmServices;
